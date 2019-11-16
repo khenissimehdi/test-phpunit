357db1ceb53667ebe6c14e55147b54ef0435acb6
Mehdi KHENISSI <mehdi.khenissi@etudiant.univ-reims.fr>
2019-11-16 12:02:57 +0100

    last commit

---
+++ src/CurrentTimeRefactored.php
@@ -0,0 +1,38 @@
+<?php
+class CurrentTime
+{
+    public function getTime():int
+    {
+        return strftime($this->getTimestamp());
+
+    }
+    public function getTimeOfDay():string
+    {
+        $chaine="";
+        $method=$this->getTime();
+        if($method<6)
+        {
+            $chaine="Night";
+        }
+        if((($method)>=6) and ($method<12))
+        {
+            $chaine="Morning";
+        }
+        if(($method>=12)and ($method<18))
+        {
+            $chaine="Noon";
+        }
+        if(($method>=18)and ($method<24))
+        {
+            $chaine="Evening";
+        }
+        return $chaine;
+
+    }
+    public function getTimestamp():int
+    {
+        return time();
+
+    } 
+
+}
