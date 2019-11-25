a09e9b7b7a5fd74e52fcb6f4a32a00259dcad3f8
Charles MALHERBE <charles.malherbe@etudiant.univ-reims.fr>
2019-11-26 00:38:31 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
-spl_autoload_register(function($className){
-	$file = __DIR__ . DIRECTORY_SEPARATOR . "src". DIRECTORY_SEPARATOR . "$className.php";
-	if(file_exists($file)){
-		require_once $file;
-	}
-});
+spl_autoload_register(
+      function ($className) {
+          $path = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $className . '.php';
+          if (file_exists($path)) {
+                require_once($path);
+          }
+      }
+);
